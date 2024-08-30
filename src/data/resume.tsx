09aefcd31d26ac6b6328be2ffefbf8f38359d96e
mhmd-aashik <aashikdevelop@gmail.com>
2024-08-30 17:29:38 +0530

    step 1

---
+++ src/data/resume.tsx
@@ -0,0 +1,309 @@
+import { Icons } from "@/components/icons";
+import { HomeIcon, NotebookIcon } from "lucide-react";
+
+export const DATA = {
+  name: "Aashik",
+  initials: "DV",
+  url: "https://dillion.io",
+  location: "San Francisco, CA",
+  locationLink: "https://www.google.com/maps/place/sanfrancisco",
+  description:
+    "Software Engineer turned Entrepreneur. I love building things and helping people. Very active on Twitter.",
+  summary: `I am a seasoned Full-Stack Developer with extensive experience in JavaScript libraries such as React, coupled with a strong foundation in UI/UX design using industry-leading tools like Figma and Adobe XD. My proficiency extends to modern frameworks including Next.js, Node.js, and MongoDB, where I focus on crafting scalable and efficient solutions tailored to meet client needs.
+
+  With a deep understanding of product management, I am passionate about leveraging technology to solve complex problems, transforming ideas into innovative, user-centric products. My holistic approach to development ensures seamless integration between front-end and back-end, creating exceptional digital experiences.`,
+  avatarUrl: "/me.png",
+  skills: [
+    "React",
+    "Next.js",
+    "Typescript",
+    "Node.js",
+    "JavaScript",
+    "Express",
+    "GraphQL",
+    "Python",
+    "Java",
+    "Postgres",
+    "Docker",
+    "Kubernetes",
+    "Java",
+    "C++",
+  ],
+  navbar: [
+    { href: "/", icon: HomeIcon, label: "Home" },
+    // { href: "/blog", icon: NotebookIcon, label: "Blog" },
+  ],
+  contact: {
+    email: "aashikdevelop@gmail.com",
+    tel: "+94 774108640",
+    social: {
+      GitHub: {
+        name: "GitHub",
+        url: "https://github.com/mhmd-aashik",
+        icon: Icons.github,
+
+        navbar: true,
+      },
+      LinkedIn: {
+        name: "LinkedIn",
+        url: "https://www.linkedin.com/in/aashikdev/",
+        icon: Icons.linkedin,
+
+        navbar: true,
+      },
+      //   X: {
+      //     name: "X",
+      //     url: "https://dub.sh/dillion-twitter",
+      //     icon: Icons.x,
+
+      //     navbar: true,
+      //   },
+      //   Youtube: {
+      //     name: "Youtube",
+      //     url: "https://dub.sh/dillion-youtube",
+      //     icon: Icons.youtube,
+      //     navbar: true,
+      //   },
+      email: {
+        name: "Send Email",
+        url: "#",
+        icon: Icons.email,
+
+        navbar: false,
+      },
+    },
+  },
+
+  work: [
+    {
+      company: "Ranga Tecnologies",
+      badges: [],
+      location: "Remote",
+      title: "Associate Full Stack Developer",
+      logoUrl: "/atomic.png",
+      start: "May 2024",
+      end: "Precent",
+      description: `I developed and maintained web applications using React, Next.js, and Node.js, designed and implemented RESTful APIs and GraphQL services, and optimized applications for maximum speed and scalability. I collaborated with clients to deliver custom solutions tailored to their business needs, utilizing technologies such as Next.js, React.js, MongoDB, TypeScript, and TailwindCSS.`,
+    },
+    {
+      company: "JSM Masterclass Experience",
+      badges: [],
+      location: "Remote",
+      title: "Fullstack Developer",
+      logoUrl: "/shopify.svg",
+      start: "January 2022",
+      end: "present",
+      description: `"Mastering Modern Web Development with a Project-Based Approach: Building a Full-Stack Enterprise Application" is a JavaScript masterclass designed to provide an immersive, hands-on learning experience. Throughout this course, you'll work on real-world projects that encompass both front-end and back-end development, utilizing the latest JavaScript frameworks and tools.
+
+      The course covers a comprehensive curriculum that delves into modern JavaScript, including ES6+ features and best practices. You'll learn front-end development with React.js, focusing on building responsive and dynamic user`,
+    },
+    {
+      company: "Mentor at Zero to Full Stack Hero by Sonny Sangha",
+      badges: [],
+      location: "Remote",
+      title: "Software Engineer",
+      logoUrl: "/nvidia.png",
+      start: "November 2022",
+      end: "Present",
+      description: `I took the lead in developing team communications and information for meetings, ensuring that all stakeholders were well-informed and aligned. By spearheading projects and thoroughly analyzing data, I identified key opportunities for improvement, driving both efficiency and effectiveness within the team.
+
+        My strong critical thinking skills enabled me to deconstruct complex problems, evaluate potential solutions, and make informed decisions that benefited the organization. Through meticulous coordination and planning, I consistently achieved results on schedule, demonstrating my ability to manage projects effectively.
+
+        In my role, I worked closely with mentees (other devs) to understand their needs and deliver exceptional coaching, tailoring my approach to meet their individual goals. By prioritizing tasks effectively and maintaining a strong work ethic, I consistently exceeded set targets, contributing to the overall success of the team.`,
+    },
+    // {
+    //   company: "Splunk",
+    //   href: "https://splunk.com",
+    //   badges: [],
+    //   location: "San Jose, CA",
+    //   title: "Software Engineer",
+    //   logoUrl: "/splunk.svg",
+    //   start: "January 2019",
+    //   end: "April 2019",
+    //   description:
+    //     "Co-developed a prototype iOS app with another intern in Swift for the new Splunk Phantom security orchestration product (later publicly demoed and launched at .conf annual conference in Las Vegas). Implemented a realtime service for the iOS app in Django (Python) and C++; serialized data using protobufs transmitted over gRPC resulting in an approximate 500% increase in data throughput.",
+    // },
+    // {
+    //   company: "Lime",
+    //   href: "https://li.me/",
+    //   badges: [],
+    //   location: "San Francisco, CA",
+    //   title: "Software Engineer",
+    //   logoUrl: "/lime.svg",
+    //   start: "January 2018",
+    //   end: "April 2018",
+    //   description:
+    //     "Proposed and implemented an internal ruby API for sending/receiving commands to scooters over LTE networks. Developed a fully automated bike firmware update system to handle asynchronous firmware updates of over 100,000+ scooters worldwide, and provide progress reports in real-time using React, Ruby on Rails, PostgreSQL and AWS EC2 saving hundreds of developer hours.",
+    // },
+    // {
+    //   company: "Mitre Media",
+    //   href: "https://mitremedia.com/",
+    //   badges: [],
+    //   location: "Toronto, ON",
+    //   title: "Software Engineer",
+    //   logoUrl: "/mitremedia.png",
+    //   start: "May 2017",
+    //   end: "August 2017",
+    //   description:
+    //     "Designed and implemented a robust password encryption and browser cookie storage system in Ruby on Rails. Leveraged the Yahoo finance API to develop the dividend.com equity screener",
+    // },
+  ],
+  education: [
+    {
+      school: "Buildspace",
+      href: "https://buildspace.so",
+      degree: "s3, s4, sf1, s5",
+      logoUrl: "/buildspace.jpg",
+      start: "2023",
+      end: "2024",
+    },
+    {
+      school: "University of Waterloo",
+      href: "https://uwaterloo.ca",
+      degree: "Bachelor's Degree of Computer Science (BCS)",
+      logoUrl: "/waterloo.png",
+      start: "2016",
+      end: "2021",
+    },
+    {
+      school: "Wilfrid Laurier University",
+      href: "https://wlu.ca",
+      degree: "Bachelor's Degree of Business Administration (BBA)",
+      logoUrl: "/laurier.png",
+      start: "2016",
+      end: "2021",
+    },
+    {
+      school: "International Baccalaureate",
+      href: "https://ibo.org",
+      degree: "IB Diploma",
+      logoUrl: "/ib.png",
+      start: "2012",
+      end: "2016",
+    },
+  ],
+  projects: [
+    {
+      title: "Chat Collect",
+      href: "https://chatcollect.com",
+      dates: "Jan 2024 - Feb 2024",
+      active: true,
+      description:
+        "With the release of the [OpenAI GPT Store](https://openai.com/blog/introducing-the-gpt-store), I decided to build a SaaS which allows users to collect email addresses from their GPT users. This is a great way to build an audience and monetize your GPT API usage.",
+      technologies: [
+        "Next.js",
+        "Typescript",
+        "PostgreSQL",
+        "Prisma",
+        "TailwindCSS",
+        "Stripe",
+        "Shadcn UI",
+        "Magic UI",
+      ],
+      links: [
+        {
+          type: "Website",
+          href: "https://chatcollect.com",
+          icon: <Icons.globe className="size-3" />,
+        },
+      ],
+      image: "",
+      video:
+        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/chat-collect.mp4",
+    },
+    {
+      title: "Magic UI",
+      href: "https://magicui.design",
+      dates: "June 2023 - Present",
+      active: true,
+      description:
+        "Designed, developed and sold animated UI components for developers.",
+      technologies: [
+        "Next.js",
+        "Typescript",
+        "PostgreSQL",
+        "Prisma",
+        "TailwindCSS",
+        "Stripe",
+        "Shadcn UI",
+        "Magic UI",
+      ],
+      links: [
+        {
+          type: "Website",
+          href: "https://magicui.design",
+          icon: <Icons.globe className="size-3" />,
+        },
+        {
+          type: "Source",
+          href: "https://github.com/magicuidesign/magicui",
+          icon: <Icons.github className="size-3" />,
+        },
+      ],
+      image: "",
+      video: "https://cdn.magicui.design/bento-grid.mp4",
+    },
+    {
+      title: "llm.report",
+      href: "https://llm.report",
+      dates: "April 2023 - September 2023",
+      active: true,
+      description:
+        "Developed an open-source logging and analytics platform for OpenAI: Log your ChatGPT API requests, analyze costs, and improve your prompts.",
+      technologies: [
+        "Next.js",
+        "Typescript",
+        "PostgreSQL",
+        "Prisma",
+        "TailwindCSS",
+        "Shadcn UI",
+        "Magic UI",
+        "Stripe",
+        "Cloudflare Workers",
+      ],
+      links: [
+        {
+          type: "Website",
+          href: "https://llm.report",
+          icon: <Icons.globe className="size-3" />,
+        },
+        {
+          type: "Source",
+          href: "https://github.com/dillionverma/llm.report",
+          icon: <Icons.github className="size-3" />,
+        },
+      ],
+      image: "",
+      video: "https://cdn.llm.report/openai-demo.mp4",
+    },
+    {
+      title: "Automatic Chat",
+      href: "https://automatic.chat",
+      dates: "April 2023 - March 2024",
+      active: true,
+      description:
+        "Developed an AI Customer Support Chatbot which automatically responds to customer support tickets using the latest GPT models.",
+      technologies: [
+        "Next.js",
+        "Typescript",
+        "PostgreSQL",
+        "Prisma",
+        "TailwindCSS",
+        "Shadcn UI",
+        "Magic UI",
+        "Stripe",
+        "Cloudflare Workers",
+      ],
+      links: [
+        {
+          type: "Website",
+          href: "https://automatic.chat",
+          icon: <Icons.globe className="size-3" />,
+        },
+      ],
+      image: "",
+      video:
+        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/automatic-chat.mp4",
+    },
+  ],
+} as const;
